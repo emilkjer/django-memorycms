9ee307b8170662210e9c605d4c317b6072404c68
Emil Kjer <emil@kjer.info>
2014-08-17 15:49:46 +0200

    update to use local.py

---
+++ backend/settings/local.py
@@ -0,0 +1,12 @@
+SECRET_KEY = ''
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '', 
+        'PASSWORD': '', 
+        'HOST': '127.0.0.1',
+        'PORT': '',
+     }
+ }
+
--- backend/settings/production.py
+++ backend/settings/production.py
@@ -8,26 +8,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*+Gkm%d#i-nyzrl1u8rr0&1ge#a%3k!6cm)l&yuyhh2spa1v14'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['memorycms.moome.net',]
-
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'memorycms',
-        'USER': 'memorycms',
-        'PASSWORD': 'tohFio2Oshiiquiobu9ru5cah7IeZei2caijootheiRahpah9uc3loov8ja2athi',
-        'HOST': '127.0.0.1',
-        'PORT': '',
-     }
- }
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/

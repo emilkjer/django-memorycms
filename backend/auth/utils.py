ed388c594f522e2bc0941a49f225ae1a94985ee8
Emil Kjer <emil@kjer.info>
2014-08-21 22:37:15 +0200

    asdf

--- backend/auth/utils.py
+++ backend/auth/utils.py
@@ -6,10 +6,10 @@ from auth.models import Token
 def json_response(response_dict, status=200):
     response = HttpResponse(json.dumps(response_dict), content_type="application/json", status=status)
     response['Access-Control-Allow-Origin'] = 'memorycms.moome.net'
-    response['Access-Control-Allow-Headers'] = 'Content-Type,*'
+    # response['Access-Control-Allow-Headers'] = 'Content-Type,*'
     response['Access-Control-Allow-Credentials'] = 'true'
+    response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
 
-    # response['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
     return response
 
 def get_token(request):
